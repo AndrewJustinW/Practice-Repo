eb721902746a68a238a93b8994f0675150d11c8c
Andrew Watson <Andrew.justinw@gmail.com>
2021-09-13 13:19:09 -0400

    Added the ability to save reading list to local storage

--- learning-context-api/src/contexts/BookContext.js
+++ learning-context-api/src/contexts/BookContext.js
@@ -1,12 +1,22 @@
-import { createContext, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid"
+import { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from "../reducers/bookReducer";
 export const BookContext = createContext();
 
 
 const BookContextProvider = (props) => {
 
-    const [books, dispatch] = useReducer(bookReducer, [])
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+
+        const localData = localStorage.getItem('booklist');
+        // If there is data in local storage, return it as the current state of the books array.
+        // If Not then just return an empty array. (which is the original state of books without local storage)
+        return localData ? JSON.parse(localData) : []
+    })
+
+    // Save Books state into local storage whenever it changes.
+    useEffect(() => {
+        localStorage.setItem('booklist', JSON.stringify(books))
+    }, [books])
 
     return (
         <BookContext.Provider value={{ books, dispatch }}>
